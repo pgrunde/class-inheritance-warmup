15ba40e205ff8e16061d5acfa43e768324083006
Peter Grunde <peter.a.grunde@gmail.com>
2014-07-30 09:43:11 -0600

    added object counter

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -6,6 +6,14 @@ class Rectangle
     attr_accessor :length
     attr_accessor :width
 
+  def self.all
+    ObjectSpace.each_object(self).to_a
+  end
+
+  def self.count
+    all.count
+  end
+
   def area(length,width)
     length * width
   end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 describe Rectangle do
   describe "Rectangle objects" do
+
+    it "can count the number of Rectangles made" do
+      one = Rectangle.new(1,2)
+      two = Rectangle.new(2,3)
+      three = Rectangle.new(3,4)
+      expect(Rectangle.count).to eq(3)
+    end
+
     it "properly attr_accessor the input args" do
       expect(Rectangle.new(5,5).length).to eq(5)
       expect(Rectangle.new(5,5).width).to eq(5)
